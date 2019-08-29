d83d3527fcd8b85c512edea4ef6d2a3b26c64ea9
bandresmf <bandresmf@wustl.edu>
2019-08-29 15:25:41 -0500

    Studio 1, Bandres, Buchanan and Abah

--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -10,8 +10,15 @@ public class Ordered {
 	public static void main(String[] args) {
 		ArgsProcessor ap = new ArgsProcessor(args);
 		int x = ap.nextInt("Value for x?");
+		System.out.println("X was " + x);
 		int y = ap.nextInt("Value for y?");
+		System.out.println("Y was " + y);
 		int z = ap.nextInt("Value for z?");
+		System.out.println("Z was " + z);
+
+		boolean isOrdered;
+isOrdered = x<y && y<z || x>y && y>z;
+		System.out.println(isOrdered);
 
 	}
 
